b8d365e67d067fa958d63d315ae86706809018e1
Chinwendu Enyinna <chinwe.promise2016@gmail.com>
2023-06-21 18:10:51 +0100

    feat: added cloudinary configuration

---
+++ utils/cloudinary.config.ts
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+import dotenv from 'dotenv'
+dotenv.config()
+// Configure Cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_USERNAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+export { cloudinary}
