504317a5ca4e5b7d86b614b853bccaf3871e9f4f
wendy <chinwe.promise2016@gmail.com>
2023-06-27 13:47:54 -0700

    fix: fix bug

--- database/config/index.ts
+++ database/config/index.ts
@@ -1,31 +1,31 @@
-import dotEnv from 'dotenv';
+import dotEnv from "dotenv";
 
-import { Dialect } from 'sequelize/types';
+import { Dialect } from "sequelize/types";
 
 dotEnv.config();
 
 const dialect = process.env.DB_DIALECT as Dialect;
 
 const envConfig = {
-    development: {
-        defaultDB: {
-            // url: process.env.DEV_DATABASE_URL,
-            url: process.env.DEV_DATABASE_URL_LOCAL,
-            dialect
-        }
+  development: {
+    defaultDB: {
+      url: process.env.DEV_DATABASE_URL,
+      // url: process.env.DEV_DATABASE_URL_LOCAL,
+      dialect,
     },
-    test: {
-        defaultDB: {
-            url: process.env.TEST_DATABASE_URL,
-            dialect
-        }
+  },
+  test: {
+    defaultDB: {
+      url: process.env.TEST_DATABASE_URL,
+      dialect,
     },
-    production: {
-        defaultDB: {
-            url: process.env.PROD_DATABASE_URL,
-            dialect
-        }
-    }
-}
+  },
+  production: {
+    defaultDB: {
+      url: process.env.PROD_DATABASE_URL,
+      dialect,
+    },
+  },
+};
 
 export default envConfig;
